d9bb5fe623f7d69cd19b0e44170f16ec2690b508
Sujatha <sujathatukula@gmail.com>
2025-03-19 17:33:56 -0700

    added remove button for each task and removing tasks on clicking on it

--- src/SimpleTodo.jsx
+++ src/SimpleTodo.jsx
@@ -7,18 +7,27 @@ function SimpleTodo(){
         "Finish Data science Project",
         "Finsih Javascript Task",
     ]);
+
+      // Function to remove a task
+   const handleRemoveTask= (indexToRemove) =>{
+       const updatedTasks= tasks.filter((_, index)=> index!== indexToRemove);
+       setTasks(updatedTasks);
+   };
+
     return(
         <div>
             <h2>Todo List</h2>
-            <ul>
-                {tasks.map((task, index) =>{
-                       return <li key={index}>{task}</li>
 
-                       /*or  {tasks.map((task, index) => (
-                        <li key={index}>{task}</li> */
-                    })}
+            <ul>
+                {tasks.map((task, index) =>(
+                    <li key={index}>
+                        {task}
+                    <button onClick={() =>handleRemoveTask(index)}>Remove Task</button>
+                    </li>
+                )  )}
             </ul>
+            
         </div>
-    )
+    );
 }
 export default SimpleTodo;
