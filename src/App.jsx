fe6e9f6c2a1371295669b1c8ee26e943331d15a8
SujathaRohit <sujathatukula@gmail.com>
2025-03-27 10:02:59 -0700

    Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 
 import Me from './assets/Me.jpg';
-import ToggleButton from './ToggleButton';
-import Counter from './CounterApp';
-import LoginAndLogout from './LoginAndLogout';
-import NameListMapMethod from './NameListMapMethod';
-import TaskList from './TaskList';
-import SimpleForm from './SimpleForm';
-import SimpleFeedback from './SimpleFeedback';
-import UnControlledComponentUseRef from './UncontrolledComponentUseRef';
-import SimpleLoginForm from './SimpleLoginForm';
-import SimpleLoginForm2 from './SimpleLoginForm2';
-import BasicStyledForm from './BasicStyledForm';
-import SimpleTodo from './SimpleTodo';
-import DynamicSimpleTodo from './DynamicSimpleTodo';
-import ExpenseTracker from './ExpenseTracker';
-import ComponentMounting from './ComponentMounting';
-import ComponentUpdating from './ComponentUpdating';
-import ComponentUnmount from './ComponentUnmount';
 import FetchUseEffectErrorHandling from './FetchUseEffectErrorHandling';
+import { ThemeProvider } from './ThemeContext';
+import Home from './Home';
 
 function App(){
   
   return(
-    <div>
-     <FetchUseEffectErrorHandling />
-    </div>
+   <ThemeProvider>
+    <Home />
+   </ThemeProvider>
   );
   
 }
